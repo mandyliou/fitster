10fdeecbcfeb51482d609ee2b615ff2ca7b9f6fc
crgcelestin <crgcelestin@gmail.com>
2023-01-10 15:18:15 -0500

    worked on tables and queries for users

--- users-api-service/migrations/001_users_migration.py
+++ users-api-service/migrations/001_users_migration.py
@@ -2,43 +2,20 @@ steps = [
     [
         # "Up" SQL statement
         """
-        CREATE TABLE dummy (
+        CREATE TABLE users (
             id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time
-                TIMESTAMP
-                NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
+            username varchar(100) not null unique,
+            first_name varchar(100) not null,
+            last_name varchar(100) not null,
+            email varchar(100) not null unique,
+            password varchar(100) not null,
+            profile_photo blob,
+            description text
         );
         """,
         # "Down" SQL statement
         """
-        DROP TABLE dummy;
-        """
-    ],
-    [
-        # "Up" SQL statement
-        """
-        CREATE TABLE big_dummy (
-            id SERIAL PRIMARY KEY NOT NULL,
-            required_limited_text VARCHAR(1000) NOT NULL,
-            required_unlimited_text TEXT NOT NULL,
-            required_date_time TIMESTAMP NOT NULL,
-            automatically_set_date_time
-                TIMESTAMP
-                NOT NULL
-                DEFAULT
-                CURRENT_TIMESTAMP,
-            required_integer INTEGER NOT NULL,
-            required_money MONEY NOT NULL
-        );
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE big_dummy;
+        DROP TABLE users;
         """
     ]
 ]
