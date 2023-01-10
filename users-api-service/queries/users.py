6437d2cdd0bd19aef0fc4c0268d9e434b05e51e9
crgcelestin <crgcelestin@gmail.com>
2023-01-10 17:00:41 -0500

    uncommented queries/users.py

--- users-api-service/queries/users.py
+++ users-api-service/queries/users.py
@@ -1,34 +1,34 @@
-# from pydantic import BaseModel, HttpUrl, ValidationError
-# from typing import Optional, List, Union
-# from queries.pool import pool
+from pydantic import BaseModel, HttpUrl, ValidationError
+from typing import Optional, List, Union
+from queries.pool import pool
 
-# class Error(BaseModel):
-#     message:str
+class Error(BaseModel):
+    message:str
 
-# class UserIn(BaseModel):
-#     username: str
-#     first_name: str
-#     last_name: str
-#     email: str
-#     password: str
-#     profile_photo: HttpUrl
-#     description: str
+class UserIn(BaseModel):
+    username: str
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+    profile_photo: HttpUrl
+    description: str
 
-# class UserOut(BaseModel):
-#     id: int
-#     username: str
-#     first_name: str
-#     last_name: str
-#     email: str
-#     password: str
-#     profile_photo: HttpUrl
-#     description: str
+class UserOut(BaseModel):
+    id: int
+    username: str
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+    profile_photo: HttpUrl
+    description: str
 
-# class UserRepository:
-#     def get_one
-#     def get_all
-#     def create
-#     def update
-#     def delete
+class UserRepository:
+    def get_one
+    def get_all
+    def create
+    def update
+    def delete
 
- #FIX WITH GROUP TO GET IT TO A POINT TO PUSH
+#FIX WITH GROUP TO GET IT TO A POINT TO PUSH
