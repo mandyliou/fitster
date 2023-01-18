26c34d6c9dae79c6e20ba2a8d8686036cfecac80
Khaled <khaled.azimi@gmail.com>
2023-01-18 12:37:04 -0800

    adding changes

--- ghi/src/UserPage.js
+++ ghi/src/UserPage.js
@@ -1,28 +1,33 @@
 import React, { useState, useEffect } from "react";
-
+import { useAuthContext } from "./auth";
 
 const UserPost = () => {
   const [posts, SetPosts] = useState([]);
-//   const [user, SetUserID] = useState([])
-
-
-let user_id = 1
+  //   const [user, SetUserID] = useState([])
+  const {token} = useAuthContext();
 
   useEffect(() => {
-      const fetchData = async () => {
-      const url = `${process.env.REACT_APP_OUTFIT_SERVICE_API_HOST}/posts/${user_id}`;
-    //   const url = `${process.env.REACT_APP_USERS_SERVICE_API_HOST}/users/`;
-      const res = await fetch(url);
-      const data = await res.json();
-      SetPosts(data);
+    const fetchData = async () => {
+      const url = `${process.env.REACT_APP_OUTFIT_SERVICE_API_HOST}/posts`;
+      //   const url = `${process.env.REACT_APP_USERS_SERVICE_API_HOST}/users/`;
+      console.log("From user post " + token);
+      if (token !== null) {
+        const res = await fetch(url, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+        const data = await res.json();
+        SetPosts(data);
+      }
     };
     fetchData();
-  }, []);
+  }, [token]);
 
   return (
     <div>
       {posts.map((post) => (
-        <div key={post.id}>{post.post_title} {post.outfit_id} {post.post_description}</div>
+        <div key={post.id}>
+          {post.post_title} {post.outfit_id} {post.post_description}
+        </div>
       ))}
     </div>
   );
