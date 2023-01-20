7c80bac9d4ae6104592b27d72241e5ce530b2819
Craig Celestin <crgcelestin@gmail.com>
2023-01-20 22:17:22 +0000

    For you-Fixed Error regarding nav bar

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -7,7 +7,8 @@ import CreateUserForm from "./CreateAccount";
 import LoginForm from "./LoginForm.js";
 import { AuthProvider, useToken } from "./auth.js";
 import UserPost from "./UserPage.js";
-import Nav from './Nav';
+// import Nav from './Nav';
+// import ForYouPage from './ForYouPage'
 
 function GetToken() {
   useToken();
@@ -41,25 +42,16 @@ export default function App() {
   return (
     <>
       <BrowserRouter>
-      <Nav>
-        <div className="container">
-          <Routes>
-            <Route path="/new-user" element={<CreateUserForm />} />
-            <Route path="/login" element={<LoginForm />} />
-          </Routes>
-          </div>
-      </Nav>
         <AuthProvider>
           <GetToken />
           <div className="container">
             <Routes>
               {/* <Route path="/" element={<MainPage />} /> */}
               <Route path="/my-profile" element={<UserPost />} />
-            <Route path="/new-user" element={<CreateUserForm />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path='posts'>
-              {/* <Route path="new" element={<CreatePost />} /> */}
-            </Route>
+              <Route path="/new-user" element={<CreateUserForm />} />
+              <Route path="/login" element={<LoginForm />} />
+              {/* <Route path="/ForYou" element={<ForYouPage />} /> */}
+              {/* <Route path="/TopFits" element={<TopFits />} /> */}
             </Routes>
           </div>
         </AuthProvider>
